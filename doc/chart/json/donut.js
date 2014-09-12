d309ed760915565920be8919b708cf1ebc4f1ea0
easylogic <cyberuls@gmail.com>
2014-09-13 02:31:29 +0900

    add colpick, slider for live coding

--- doc/chart/json/donut.js
+++ doc/chart/json/donut.js
@@ -7,7 +7,7 @@ chart("#chart", {
         { key : 'yellow', 	name : 46, 	value : 15 },
         { key : 'red',		name : 25, 	value : 6 },
         { key : 'black',	name : 18, 	value : 10 },
-        { key : 'blue',		name : 10, 	value : 5 },
+        { key : 'blue',		name : 10, 	value : 5 }
     ],
     brush : [{
         type : 'donut',
