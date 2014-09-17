be8e36c79adbd480ad0eda390e17c549d262ecc2
seogi1004 <alvin@jennifersoft.com>
2014-09-17 12:43:57 +0900

    manual example changed!!!  - all charts

--- doc/chart/json/mixed1.js
+++ doc/chart/json/mixed1.js
@@ -3,32 +3,26 @@ var chart = jui.include('chart.basic');
 chart("#chart-content", {
     width: 400,
     height : 400,
-    padding: { top : 0 },
     data : [
-        { name : 2, value : 15, domain: "week1" },
-        { name : 13, value : 6, domain: "week2" },
-        { name : 8, value : 10, domain: "week3" },
-        { name : 2, value : 5, domain: "week4" },
+        { quarter : "1Q", sales : 49, profit : 35 },
+        { quarter : "2Q", sales : 38, profit : 30 },
+        { quarter : "3Q", sales : 10, profit : 4 },
+        { quarter : "4Q", sales : 30, profit : 24 }
     ],
     grid : {
         x : {
-            target : "domain",
+            target : "quarter",
             line : true
         },
-        y :
-        {
-            type : 'range',
-            target : [ "name", "value" ],
+        y : {
+            type : "range",
+            target : "sales",
             step : 10
         }
     },
-    series : {
-        name : { text : "test" },
-        value : { text : "test" }
-    },
     brush : [
-        { type : "column", target : "value", colors : [ "#7977C2" ] },
-        { type : "line", symbol: "curve", target : "name", colors : [ "#7BBAE7" ] },
-        { type : "scatter", size: 7, target : "name", colors : [ "#7BBAE7" ] }
+        { type : "column", target : "profit", colors : [ "#7977C2" ] },
+        { type : "line", target : "sales", colors : [ "#7BBAE7" ], symbol: "curve" },
+        { type : "scatter", target : "sales", colors : [ "#7BBAE7" ], size: 7 }
     ]
 }).render();
