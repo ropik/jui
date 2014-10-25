0c3b6f505c6f6d164abfd8d206c3cccddfc9da2e
seogi1004 <alvin@jennifersoft.com>
2014-10-26 02:27:59 +0900

    manual changed!!!  - update jui library

--- doc/chart/json/realtime_area.js
+++ doc/chart/json/realtime_area.js
@@ -1,58 +1,20 @@
 var chart = jui.include("chartx.realtime");
 var time = jui.include('util.time');
-var realtimeIndex = 0;
-
-var getRealtimeData = function(min) {
-    var start = time.add(new Date(), time.minutes, -5),
-        data = [];
-
-    for(var i = 0; i < min * 60; i++) {
-        data.push({
-            time : time.add(start, time.seconds, i + 1),
-            s1 : Math.sin(realtimeIndex / 20) - 4,
-            s2 : Math.sin(realtimeIndex / 10) * 2.5,
-            s3 : Math.sin(realtimeIndex / 15) + 5
-        });
-
-        realtimeIndex++;
-    }
-
-    return data;
-}
-
-var runRealtimeData = function(realtime) {
-    setInterval(function() {
-        realtime.append({
-            time : new Date(),
-            s1 : Math.sin(realtimeIndex / 20) - 4,
-            s2 : Math.sin(realtimeIndex / 10) * 2.5,
-            s3 : Math.sin(realtimeIndex / 15) + 5
-        });
-
-        realtimeIndex++;
-    }, 1000);
-}
 
 var c = chart("#chart", {
     data : getRealtimeData(5),
-    brush : {
+    brush : [{
+        type : "line",
+        target : [ "s1", "s2", "s3" ]
+    }, {
         type : "area",
         target : [ "s1", "s2", "s3" ]
-    },
+    }],
     widget : [{
         type : "title",
         text : "Realtime Sample"
     }, {
         type : "legend"
-    }, {
-        type : "cross",
-        format : function(d) {
-            if(typeof(d) == "number") {
-                return Math.round(d);
-            } else {
-                return time.format(d, "hh:mm:ss");
-            }
-        }
     }]
 });
 
--- doc/chart/json/realtime_line.js
+++ doc/chart/json/realtime_line.js
@@ -1,37 +1,5 @@
 var chart = jui.include("chartx.realtime");
 var time = jui.include('util.time');
-var realtimeIndex = 0;
-
-var getRealtimeData = function(min) {
-    var start = time.add(new Date(), time.minutes, -5),
-        data = [];
-
-    for(var i = 0; i < min * 60; i++) {
-        data.push({
-            time : time.add(start, time.seconds, i + 1),
-            s1 : Math.sin(realtimeIndex / 20) - 4,
-            s2 : Math.sin(realtimeIndex / 10) * 2.5,
-            s3 : Math.sin(realtimeIndex / 15) + 5
-        });
-
-        realtimeIndex++;
-    }
-
-    return data;
-}
-
-var runRealtimeData = function(realtime) {
-    setInterval(function() {
-        realtime.append({
-            time : new Date(),
-            s1 : Math.sin(realtimeIndex / 20) - 4,
-            s2 : Math.sin(realtimeIndex / 10) * 2.5,
-            s3 : Math.sin(realtimeIndex / 15) + 5
-        });
-
-        realtimeIndex++;
-    }, 1000);
-}
 
 var c = chart("#chart", {
     data : getRealtimeData(5),
