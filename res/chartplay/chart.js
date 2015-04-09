4d347073ddb24448f128cd34efc7e372ec1e2aa5
seogi1004 <alvin@jennifersoft.com>
2015-04-09 10:59:17 +0900

    Chart play updated

--- res/chartplay/chart.js
+++ res/chartplay/chart.js
@@ -36,8 +36,7 @@ var code_list = [
     { type: "etc", title : "Use SVG Icons",  code : "use_svg_icons.js" },
 
     // grid line style
-    { type: "grid", title : "Base Grid", code : "grid_base.js" },
-    { type: "grid", title : "Solid Line", code : "grid_solid.js", hide : true },
+    { type: "grid", title : "Solid Line", code : "grid_solid.js" },
     { type: "grid", title : "Solid + Gradient", code : "grid_solid_gradient.js" },
     { type: "grid", title : "Solid + Rect", code : "grid_solid_rect.js" },
     { type: "grid", title : "Dashed Line", code : "grid_dashed.js" },
