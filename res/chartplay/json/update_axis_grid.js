c72c8f6f39e869f8130b931a6f00022d3e273fa8
seogi1004 <seogi777@gmail.com>
2015-06-10 18:51:06 +0900

    차트 샘플 수정  - update_axis_grid.js

--- res/chartplay/json/update_axis_grid.js
+++ res/chartplay/json/update_axis_grid.js
@@ -18,6 +18,10 @@ var c = chart("#chart", {
             { quarter : "3Q", sales : 8300, profit : 6700 },
             { quarter : "4Q", sales : 5200, profit : 4800 }
         ]
+    },
+    brush: {
+        type: "column",
+        target: [ "sales", "profit" ]
     }
 });
 
@@ -26,14 +30,6 @@ setTimeout(function() {
     var axis = c.axis(0);
 
     axis.updateGrid("y", {
-        type : "block",
-        domain : "quarter",
-        line : true
-    });
-
-    axis.updateGrid("x", {
-        type : "range",
-        domain : [ 0, 10000 ],
-        step : 4
+        domain : [ 0, 20000 ]
     });
 }, 5000);
