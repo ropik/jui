2c5488a6b89eee8374542a107361efbfffd24e5e
seogi1004 <seogi777@gmail.com>
2015-07-15 19:29:32 +0900

    #331 스크롤 컴포넌트 개발 중...

---
+++ js/ui/scroll.js
@@ -0,0 +1,23 @@
+jui.defineUI("ui.scroll", [ "jquery" ], function($) {
+
+    /**
+     * @class ui.scroll
+     *
+     * @extends core
+     * @alias Scroll
+     * @requires jquery
+     */
+    var UI = function() {
+        this.init = function() {
+
+        }
+    }
+
+    UI.setup = function() {
+        return {
+
+        }
+    }
+
+    return UI;
+});
